6c96c24488f26a5e592c6db3729ba275ab162bf1
fahreddingolcuk <fahreddingolcuk@gmail.com>
2021-05-15 14:27:10 +0300

    Initial Commit

---
+++ Noomitpad/NoomitpadApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoomitpadApp.swift
+//  Noomitpad
+//
+//  Created by Fahreddin Gölcük on 15.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NoomitpadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
