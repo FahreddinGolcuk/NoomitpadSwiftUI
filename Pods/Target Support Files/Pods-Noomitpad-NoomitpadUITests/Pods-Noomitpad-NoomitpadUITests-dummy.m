45ff2fe8e08491e85df9815e2047317c03d1ef6f
fahreddingolcuk <fahreddingolcuk@gmail.com>
2021-05-16 14:40:51 +0300

    commit 2

---
+++ Pods/Target Support Files/Pods-Noomitpad-NoomitpadUITests/Pods-Noomitpad-NoomitpadUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Noomitpad_NoomitpadUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Noomitpad_NoomitpadUITests
+@end
---
+++ Pods/Target Support Files/Pods-Noomitpad/Pods-Noomitpad-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Noomitpad : NSObject
+@end
+@implementation PodsDummy_Pods_Noomitpad
+@end
